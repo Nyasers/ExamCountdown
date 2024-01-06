42f79ba458946642fcda1370e904edc4d55c21c2
Nyaser Project <admin@nyaser.tk>
2024-01-06 17:26:42 +0000

    fix bugs

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(compression());
 app.use('/', async (req, res) => {
     res.charset = 'utf-8';
-    res.writeHead(200);
+    res.status(200);
+    res.type('text/html');
     res.end(await fetchHTML(0));
 });
 app.use('/src/:file', src);
