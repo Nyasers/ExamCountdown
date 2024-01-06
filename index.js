e0bc0b005afbdfba109ee7fa62eae5a36b2b2013
Nyaser Project <admin@nyaser.tk>
2024-01-06 17:27:57 +0000

    fux bugs

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import { src, fetchHTML } from './src/index.mjs';
 const app = express();
 
 app.use(compression());
+app.use('/src/:file', src);
 app.use('/', async (req, res) => {
     res.charset = 'utf-8';
     res.status(200);
     res.type('text/html');
     res.end(await fetchHTML(0));
 });
-app.use('/src/:file', src);
 
 app.listen(process.env.PORT || '3000');
