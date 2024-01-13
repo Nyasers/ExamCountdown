94598441df56c6f3b84d34ad1940ceea4cc730ed
Nyaser Project <admin@nyaser.tk>
2024-01-13 15:25:45 +0000

    update.cmd: fix bugs

--- src/cmd/update.cmd
+++ src/cmd/update.cmd
@@ -1,1 +1,1 @@
-powershell "$t='$ec_temp.zip';Invoke-WebRequest -Uri https://ec.nyaser.tk/zip -OutFile $t;Expand-Archive -Path $t -DestinationPath %~dp0..\..\..\..\.. -Force;Remove-Item $file"||cmd/k
+powershell "$t='$ec_temp.zip';Invoke-WebRequest -Uri https://ec.nyaser.tk/zip -OutFile $t;Expand-Archive -Path $t -DestinationPath '%~dp0..\..\..\..\..' -Force;Remove-Item $file"||cmd/k
