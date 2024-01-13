f840c99d62bdf7592da1b95cbee50c595de4aaa5
Nyaser Project <admin@nyaser.tk>
2024-01-13 04:45:28 +0000

    deploy test: fix bugs

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 import { fetchFile } from './src/index.mjs';
 import fs from 'fs';
 
-fs.rmdirSync('./dist', { recursive: true, force: true });
+if (fs.existsSync('./dist')) fs.rmdirSync('./dist', { recursive: true, force: true });
 fs.mkdirSync('./dist');
 
 fs.writeFileSync('./dist/index.html', await fetchFile('index.html'));
