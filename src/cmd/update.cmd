40af52b20f6e436bdb01ea735b9b3502c5e92cb1
Nyaser Project <admin@nyaser.tk>
2024-01-13 17:32:55 +0000

    update.cmd: 1 byte minified XD

--- src/cmd/update.cmd
+++ src/cmd/update.cmd
@@ -1,1 +1,1 @@
-powershell "$t=[System.IO.Path]::GetTempFileName();Invoke-WebRequest -Uri https://ec.nyaser.tk/zip -OutFile $t;Expand-Archive -Force -Path $t -DestinationPath %~dp0..\..\..\..\..;ri $t;"||cmd/k
+powershell "$t=[System.IO.Path]::GetTempFileName();Invoke-WebRequest -Uri https://ec.nyaser.tk/zip -OutFile $t;Expand-Archive -Force -Path $t -DestinationPath %~dp0..\..\..\..\..;ri $t"||cmd/k
