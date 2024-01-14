0983f4fd4aa4a2058c05cba3ca29516ebbd2676a
Nyaser Project <admin@nyaser.tk>
2024-01-14 02:54:37 +0000

    update.js

--- src/js/extension/update.js
+++ src/js/extension/update.js
@@ -7,9 +7,8 @@ ec.update.check = () => {
   else return (new Date().getTime() - ec.update.version.getTime()) / 8.64e7;
 };
 ec.update.notice = () => {
-  let content = `Nyaser：本地文件有新的版本可用(${ec.version ? ec.version.toLocaleString('zh-cn') : undefined
+  let content = `Nyaser：本地文件有更新：(${ec.version ? ec.version.toLocaleString('zh-cn') : undefined
     } -> ${ec.update.version ? ec.update.version.toLocaleString('zh-cn') : undefined
-    })，<br>请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0]+'update.cmd')} 进行更新，<br>或用浏览器打开 ${ec.update.href
-    } 以下载更新包。<br>${ec.update.msg}`;
+    })<br>${ec.update.msg}`;
   $("body").append(`<center><big><big>${content}</big></big></center>`);
 };
