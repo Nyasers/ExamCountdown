c977c3ac76e143483a5cd49dc7e51db4e9e4a16f
Nyaser Project <admin@nyaser.tk>
2024-01-14 07:55:52 +0000

    chg version

--- src/js/version.js
+++ src/js/version.js
@@ -1,5 +1,5 @@
 !function () {
-  let version = new Date("2024/01/14 14:57");
+  let version = new Date("2024/01/14 15:55");
   if (typeof ec.update == "undefined") ec.version = version;
   else ec.update.version = version;
 }();
