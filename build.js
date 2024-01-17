a5da8a72448c271948494f31f82d0963ebfc697e
Nyaser Project <admin@nyaser.tk>
2024-01-17 08:38:45 +0000

    DeprecationWarning

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 import { fetchFile } from './src/index.mjs';
 import fs from 'fs';
 
-if (fs.existsSync('./dist')) fs.rmdirSync('./dist', { recursive: true, force: true });
+if (fs.existsSync('./dist')) fs.rmSync('./dist', { recursive: true, force: true });
 fs.mkdirSync('./dist');
 
 fs.copyFileSync('./_redirects', './dist/_redirects');
