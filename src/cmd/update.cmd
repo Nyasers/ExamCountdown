b9198d4bcd2ca9bc2bc059d53c6279b28f456b92
Nyaser Project <admin@nyaser.tk>
2024-01-17 05:07:17 +0000

    update.cmd: use alias

--- src/cmd/update.cmd
+++ src/cmd/update.cmd
@@ -1,1 +1,1 @@
-powershell "$t=[System.IO.Path]::GetTempFileName()+'.zip';Invoke-WebRequest -Uri https://ec.nyaser.tk/u -OutFile $t;Expand-Archive -Path $t -DestinationPath %~dp0 -Force;ri $t"||cmd/k
+powershell "$t=[System.IO.Path]::GetTempFileName()+'.zip';iwr -Uri https://ec.nyaser.tk/u -OutFile $t;Expand-Archive -Path $t -DestinationPath %~dp0 -Force;ri $t"||cmd/k
