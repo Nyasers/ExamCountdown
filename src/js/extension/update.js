e0217380135b616f3032954e5680eb06a912ed51
Nyaser Project <admin@nyaser.tk>
2024-01-26 05:07:58 +0000

    update.js: fix an error url

--- src/js/extension/update.js
+++ src/js/extension/update.js
@@ -1,5 +1,5 @@
 ec.update = {};
-ec.update.href = "https://ec.nyaser.tk/zip";
+ec.update.href = "https://ec.nyaser.tk/z";
 ec.update.check = () => {
   if (typeof ec.version == "undefined") return Infinity;
   else if (typeof ec.update.version == "undefined") return false;
