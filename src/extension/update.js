2c77e256512eace13f1d4b10caeba2612de0e9ac
Nyaser Project <admin@nyaser.tk>
2024-01-31 16:17:43 +0000

    rebuilding: bugfix

--- src/extension/update.js
+++ src/extension/update.js
@@ -2,7 +2,7 @@
 
 export default {
   href: "https://ec.nyaser.tk/zip",
-  msg: `请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0] + 'update.cmd')} 进行更新，<br>或用浏览器打开 ${ec.update.href} 下载压缩包手动更新。`,
+  msg: location.protocol == 'file:' ? `请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0] + 'update.cmd')} 进行更新，<br>或用浏览器打开 ${ec.update.href} 下载压缩包手动更新。` : 'Update is unavailable online.',
   check: () => {
     if (typeof ec.version == "undefined") return Infinity;
     else if (typeof ec.update.version == "undefined") return false;
