dfa2fe49644432d74a13163357f419aacc49b3ca
Nyaser Project <admin@nyaser.tk>
2024-01-31 10:15:25 +0000

    rebuilding

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import $ from 'jquery';
+import './define.js';
 import main from './base/main.js';
 import exams from './extension/exams.js';
 import hitokoto from './extension/hitokoto.js';
 import '../cache/base.css';
 import '../cache/extension.css';
-
-// const { $, ec } = window;
-window.$ = $;
-window.ec = {};
 
 ec.exams.json.push(exams);
 ec.exams.build();
