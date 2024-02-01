7d05480cf78ee3eda594c15d6b0baf4e81139849
Nyaser Project <admin@nyaser.tk>
2024-02-01 01:40:04 +0000

    BUGFIX

--- src/extension/update.js
+++ src/extension/update.js
@@ -2,12 +2,12 @@
 
 export default {
   href: "https://ec.nyaser.tk/zip",
-  msg: location.protocol == 'file:' ? `请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0] + 'update.cmd')} 进行更新，<br>或用浏览器打开 ${ec.update.href} 下载压缩包手动更新。` : 'Update is unavailable online.',
+  msg: location.protocol == 'file:' ? `请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0] + 'update.cmd')} 进行更新，<br>或用浏览器打开 ${this.href} 下载压缩包手动更新。` : 'Update is unavailable online.',
   check: () => {
     if (typeof ec.version == "undefined") return Infinity;
-    else if (typeof ec.update.version == "undefined") return false;
-    else if (ec.version.getTime() == ec.update.version.getTime()) return false;
-    else return (new Date().getTime() - ec.update.version.getTime()) / 8.64e7;
+    else if (typeof this.version == "undefined") return false;
+    else if (ec.version.getTime() == this.version.getTime()) return false;
+    else return (new Date().getTime() - thiS.version.getTime()) / 8.64e7;
   },
   notice: () => {
     let content = `Nyaser: 更新可用 (${ec.version ? ec.version.toLocaleString('zh-cn') : undefined
