607fef2e85e81c88e885995b3e5c23182de72544
Nyaser Project <admin@nyaser.tk>
2024-02-04 15:10:24 +0000

    bugfix

--- terser.config.js
+++ terser.config.js
@@ -10,7 +10,7 @@ export default {
     compress: {
         passes: 3,
         arguments: true,
-        drop_console: true,
+        // drop_console: true,
         booleans_as_integers: true,
         unsafe: true,
         unsafe_arrows: true,
@@ -18,7 +18,7 @@ export default {
         unsafe_comps: true,
         unsafe_proto: true,
         unsafe_math: true,
-        unsafe_methods: true,
-        unsafe_undefined: true,
+        // unsafe_methods: true,
+        // unsafe_undefined: true,
     },
 };
