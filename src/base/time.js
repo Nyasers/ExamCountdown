4dace602e3c17d78953d39d960f7c431eaabf3b8
Nyaser Project <admin@nyaser.tk>
2024-02-05 15:47:04 +0000

    try

--- src/base/time.js
+++ src/base/time.js
@@ -2,14 +2,14 @@ import $ from 'jquery';
 
 const url = 'https://time.is/?c=d3Y-3m-3dXtH2i2s.MXfmtXc2Xo480Xz1Xa1Xb51ea29.4e4185.28571f.2d99db.80265.1bb85e.1c3b23Xw1Xv20240205Xh0Xi1XZ1XmXuXB0Xs0XT0&l=zh';
 const iframe = $('<iframe>').attr('src', url);
+$('body').append(iframe);
 globalThis.iframe = iframe;
+globalThis.idocument = iframe[0].contentDocument;
 
-function getTimeStr(selector) {
-    var document = $(selector)[0].contentDocument;
-    return $(document).find('#dd').textContent + 'T' + $(document).find('#clock').textContent;
+function getTimeStr(idocument) {
+    return $(idocument).find('#dd').textContent + 'T' + $(idocument).find('#clock').textContent;
 }
 
 export default function () {
-    $('body').append(iframe);
-    return getTimeStr(iframe);
+    return getTimeStr(idocument);
 }
