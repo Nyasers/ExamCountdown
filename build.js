a2d754ec8ae9cd9947858eb438d825280b512843
Nyaser Project <admin@nyaser.tk>
2024-03-23 10:59:41 +0000

    build.js install path: chg to SteamLibrary

--- build.js
+++ build.js
@@ -37,8 +37,7 @@ async function packZip(files, destination) {
 
 async function postMake() {
     const json = path.resolve('dist/project.json');
-    const pathto = '/wallpaper_engine/projects/myprojects/ExamCountdown';
-    //const pathto = '/Wallpaper/projects/defaultprojects/ExamCountdown';
+    const pathto = '/SteamLibrary/steamapps/common/wallpaper_engine/projects/myprojects/ExamCountdown';
     const install = [
         { filename: 'index.html', data: { name: path.resolve(pathto, 'index.html') } },
         { filename: 'project.json', data: { name: path.resolve(pathto, 'project.json') } },
