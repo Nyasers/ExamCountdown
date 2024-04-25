fd7827093a9a17f414f90079045e5759fc074737
Nyaser Project <admin@nyaser.tk>
2024-04-25 16:08:37 +0000

    bingwallpaper.js more new tricks

--- src/extension/bingwallpaper.js
+++ src/extension/bingwallpaper.js
@@ -1,11 +1,11 @@
 const $ = globalThis.$;
 // import { $ } from 'jquery';https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main/data/zh-CN_update.json
 function fetchAndApply(index = 0) {
-    $.getJSON('https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main/data/zh-CN_update.json')
+    $.getJSON(`https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main/data/zh-CN_${index > 7 ? 'all' : 'update'}.json`)
         .then((data) => {
             console.log(data);
             let origin = 'https://cn.bing.com';
-            let url = data.images[index].url;
+            let url = data[index > 7 ? 'data' : 'images'][index].url;
             document.body.style.backgroundImage = `url('${origin + url}')`;
         });
 }
