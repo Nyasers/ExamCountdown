c9da6c1ea98e525a6e6664c6f4a5f0b26d8e36da
Nyaser Project <admin@nyaser.tk>
2024-05-09 08:05:16 +0000

    trying (tired)

--- src/base/applyImage.js
+++ src/base/applyImage.js
@@ -2,29 +2,13 @@ import { getAverageColor } from "./color.js";
 import { getContrast, rgbArrayToHex } from "./color.js";
 import { themeColor } from "./themecolor.js";
 
-async function getThemeColor(img) {
-    themeColor(img, async (themeColors) => {
-        var colors = [themeColors[5], themeColors[6]];
-        let aveColor = await getAverageColor(colors);
-        return aveColor;
-    });
-}
-
-/*
-var blob = new Blob([`self.addEventListener('message', ${getThemeColor.toString()}, false);`]);
-var url = URL.createObjectURL(blob);
-var worker = new Worker(url);
-worker.addEventListener('message', function (color) {
-    setColors(color);
-    worker.terminate();
-})
-*/
-
 async function applyImage(img) {
     document.body.style.backgroundImage = `url(${img.src})`;
-    var themeColor = await getThemeColor(img);
-    setColors(themeColor);
-    //worker.postMessage(() => new Image(img));
+    themeColor(img, async (themeColors) => {
+        let colors = [themeColors[5], themeColors[6]];
+        let aveColor = await getAverageColor(colors);
+        await setColors(aveColor);
+    });
 }
 
 async function setColors(themeColorRgbArray) {
@@ -36,7 +20,7 @@ async function setColors(themeColorRgbArray) {
 }
 
 export function applyImageUrl(url) {
-    var preloader = new Image();
+    let preloader = new Image();
     preloader.onload = async () => await applyImage(preloader);
     preloader.src = url;
     preloader.setAttribute('crossOrigin', '');
