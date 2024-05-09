fd65434fbba2cacdd9f02a721808f99e056dece9
Nyaser Project <admin@nyaser.tk>
2024-05-09 05:35:42 +0000

    localise wallpaper

--- src/base/index.js
+++ src/base/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import time from './time.js';
 import main from './main.js';
 import extension from './extension.js';
+import fetchBW from "../base/bingwallpaper.js";
 import '../../cache/index.css';
 
 // Expose $ and ec
@@ -18,3 +19,12 @@ ec.version = new Date(VERSION);
 // Fetch extensions
 ec.extension = extension;
 setTimeout(ec.extension.fetch);
+
+// Fetch Bing Wallpaper
+globalThis.fetchBW = fetchBW;
+if (location.protocol !== 'file:') {
+  setTimeout(() => fetchBW(0, '1920x1080.webp'));
+} else if (document.body.style.backgroundImage == ''
+  || document.body.style.backgroundImage == 'url("file:///C%3A/Windows/Web/Wallpaper/Windows/img0.jpg")') {
+  setTimeout(() => fetchBW());
+}
--- src/extension/index.js
+++ src/extension/index.js
@@ -1,8 +1,5 @@
-// import invert from 'invert-color';
 import update from './update.js'
 import hitokoto from "./hitokoto.js";
-import fetchBW from './bingwallpaper.js';
-// import { themeColor } from './themecolor.js';
 import '../../cache/extension.css';
 
 // Avoid duplicate runs
@@ -21,16 +18,3 @@ setTimeout(() => {
 // Init hitokoto
 $("ul#main").append($('<li class="hitokoto"></li>'));
 ec.hitokoto = hitokoto;
-
-// Fetch Bing Wallpaper
-globalThis.fetchBW = fetchBW;
-if (location.protocol !== 'file:') {
-  setTimeout(() => fetchBW(0, '1920x1080.webp'));
-} else if (document.body.style.backgroundImage == ''
-  || document.body.style.backgroundImage == 'url("file:///C%3A/Windows/Web/Wallpaper/Windows/img0.jpg")') {
-  setTimeout(() => fetchBW());
-}
-
-// Color
-// globalThis.themeColor = themeColor;
-// globalThis.invertColor = invert;
