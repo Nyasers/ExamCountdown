2710452ab13931e016480f47eb2a27f1e8ebab48
Nyaser Project <admin@nyaser.tk>
2024-05-09 03:54:05 +0000

    debug

--- src/extension/bingwallpaper.js
+++ src/extension/bingwallpaper.js
@@ -35,7 +35,7 @@ async function applyImage(img) {
 }
 
 async function getAverageColor(themeColors) {
-    var colors = themeColors.reduce(2).reduceRight(2);
+    var colors = [themeColors[2], themeColors[3], themeColors[4], themeColors[5]];
     var len = colors.length;
     var sum = [0, 0, 0];
     var res = [0, 0, 0];
