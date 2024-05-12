2da155e214bc532b3b15ac3484312e5532f91fe9
Nyaser Project <admin@nyaser.tk>
2024-05-12 15:16:16 +0000

    bingwallpaper.js anti cache

--- src/base/bingwallpaper.js
+++ src/base/bingwallpaper.js
@@ -1,15 +1,23 @@
 import { $ } from 'jquery';
 
 const api = 'https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main';
-const bing = 'https://cn.bing.com'; //s.cn.bing.net
+const bing = 'https://s.cn.bing.net'; //cn.bing.com
 
 async function getURLBase(index = 0) {
-    const data = await $.getJSON(`${api}/data/zh-CN_${index > 7 ? 'all' : 'update'}.json?_=${new Date().getTime()}`)
-    const images = data[index > 7 ? 'data' : 'images'];
-    if (index >= images.length)
-        throw new Error(`Trying to get the ${index + 1} from ${images.length} elements.`);
-    let image = images[index];
-    return bing + image.urlbase;
+    const url = `${api}/data/zh-CN_${index > 7 ? 'all' : 'update'}.json`;
+    $.ajax({
+        type: "GET",
+        url,
+        cache: false,
+        dataType: "json",
+        success: function (data) {
+            const images = data[index > 7 ? 'data' : 'images'];
+            if (index >= images.length)
+                throw new Error(`Trying to get the ${index + 1} from ${images.length} elements.`);
+            let image = images[index];
+            return bing + image.urlbase;
+        }
+    });
 }
 
 async function fetchBW(index = 0, ext = 'UHD.jpg') {
