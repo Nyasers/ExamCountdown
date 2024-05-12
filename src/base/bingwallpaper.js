f65eb91792dda0216e9cab1a83786199298ed7b5
Nyaser Project <admin@nyaser.tk>
2024-05-12 15:40:14 +0000

    debug

--- src/base/bingwallpaper.js
+++ src/base/bingwallpaper.js
@@ -5,26 +5,22 @@ const bing = 'https://s.cn.bing.net'; //cn.bing.com
 
 async function getURLBase(index = 0) {
     const url = `${api}/data/zh-CN_${index > 7 ? 'all' : 'update'}.json`;
-    const ref = await $.ajax({
+    const data = await $.ajax({
         type: "GET",
         url,
-        async: false,
         cache: false,
-        dataType: "json",
-        success: function (data) {
-            // console.log(data);
-            const images = data[index > 7 ? 'data' : 'images'];
-            // console.log(images);
-            if (index >= images.length)
-                throw new Error(`Trying to get the ${index + 1} from ${images.length} elements.`);
-            let image = images[index];
-            // console.log(image);
-            const urlbase = bing + image.urlbase;
-            console.log(urlbase);
-            return urlbase;
-        }
+        dataType: "json"
     });
-    console.log(ref);
+    // console.log(data);
+    const images = data[index > 7 ? 'data' : 'images'];
+    // console.log(images);
+    if (index >= images.length)
+        throw new Error(`Trying to get the ${index + 1} from ${images.length} elements.`);
+    let image = images[index];
+    // console.log(image);
+    const urlbase = bing + image.urlbase;
+    console.log(urlbase);
+    return urlbase;
 }
 
 async function fetchBW(index = 0, ext = 'UHD.jpg') {
