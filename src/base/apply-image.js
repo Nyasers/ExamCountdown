920e08d123797979cf10df2307bd3e0d5b6f82e9
Nyaser Project <admin@nyaser.tk>
2024-05-31 15:39:01 +0000

    apply-image.js quick response for local images

--- src/base/apply-image.js
+++ src/base/apply-image.js
@@ -11,12 +11,16 @@ ImageLoaderWorker.addEventListener('message', event => {
 })
 
 async function applyImage(imageData) {
-    document.body.style.backgroundImage = `url(${imageData.objectURL})`;
+    setBackground(imageData.objectURL);
     await (async (themeColors) => {
         let colors = [themeColors[themeColors.length - 3], themeColors[themeColors.length - 2]];
         let aveColor = await getAverageColor(colors);
         await setColors(aveColor);
     })(imageData.themeColors);
+}
+
+function setBackground(url) {
+    document.body.style.backgroundImage = `url(${url})`;
 }
 
 async function setColors(themeColorRgbArray) {
@@ -28,5 +32,6 @@ async function setColors(themeColorRgbArray) {
 }
 
 export default function applyImageUrl(url) {
+    if (url.startsWith('file:///')) setBackground(url);
     ImageLoaderWorker.postMessage(url);
 }
