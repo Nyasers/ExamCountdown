109cf5d695549e95d648a83f9e556b380fa8555f
Nyaser Project <admin@nyaser.tk>
2024-06-10 09:33:11 +0000

    ehehe

--- src/extension/version.js
+++ src/extension/version.js
@@ -1,4 +1,5 @@
-let input = [
+var o = [];
+[
     {
         d: '2024-06-10T15:00',
         m: "更新 2024 年中考及会考时间"
@@ -7,19 +8,8 @@ let input = [
         d: '2024-06-10T16:00',
         m: '更新高考时间'
     }
-]
-
-
-let output = [];
-input.forEach(version => {
-    let build = {
-        date: new Date(version.d ?? 0),
-        msg: version.m ?? ''
-    }
-    output.join(build);
-});
-output.sort((left, right) =>
-    left.date.getTime() ?? 0 - right.date.getTime() ?? 0
-);
-
-export default output;
+].forEach(v => o = o.concat({
+    date: new Date(v.d ?? 0),
+    msg: v.m ?? ''
+}));
+export default o.sort((a, b) => a.date.getTime() - b.date.getTime());
