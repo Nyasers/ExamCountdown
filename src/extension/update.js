8606569471149a74bc4aea4567a0efd4ce7fdb99
Nyaser Project <admin@nyaser.tk>
2024-06-10 08:51:46 +0000

    try a new update.js

--- src/extension/update.js
+++ src/extension/update.js
@@ -1,27 +1,31 @@
 import version from './version.js';
 
+function getVersionString(date) {
+  return date?.toJSON()?.split(':00.')[0] ?? undefined;
+}
+
 export default {
-  msg: location.protocol == 'file:'
-    ? `请运行 ${decodeURI(location.href.split('file:///')[1].split('index.html')[0] + 'update.cmd').replaceAll('/', '\\')
-    } 进行更新，<br>或用浏览器打开 https://ec.nyase.ru/zip 下载压缩包手动更新。`
-    : undefined,
-  check: function () {
-    const ec = globalThis.ec;
+  msg: (function () {
+    const projectRoot = location.protocol == 'file:' ? decodeURI(location.href.split('file:///')[1].split('index.html')[0]).replaceAll('/', '\\') : undefined;
+    return projectRoot
+      ? `1.运行 ${projectRoot + 'update.cmd'}<br>`
+      + `2.用浏览器打开 https://ec.nyase.ru/zip 下载压缩包，解压到${projectRoot}<br>`
+      : '才不需要更新呢，哼！<br>';
+  }).bind(globalThis)(),
+  check: (function () {
     if (typeof ec.version == "undefined") return Infinity;
     else if (typeof ec.update.version == "undefined") return false;
     else if (ec.version.getTime() == ec.update.version.getTime()) return false;
     else return (Time().getTime() - ec.update.version.getTime()) / 8.64e7;
-  },
-  notice: function () {
-    const ec = globalThis.ec;
-    let content = `Nyaser: 更新可用 (${ec.version ? ec.version.toJSON().split(':00.')[0] : undefined
-      } -> ${ec.update.version ? ec.update.version.toJSON().split(':00.')[0] : undefined
-      })<br>${ec.update.msg}`;
+  }).bind(globalThis),
+  notice: (function () {
+    let content = `Nyaser: 更新可用 (${getVersionString(ec.version)} -> ${getVersionString(ec.update.version)})`;
+    content += `<br>--更新方法--<br>${ec.update.msg}`;
     version.forEach(v => {
       if (ec.version.getTime() < new Date(v.date).getTime()) {
-        content += `<br>${v.date}: ${v.msg}`;
+        content += `<br>${v.date}: ${v.msg} `;
       }
     });
     $("body").append(`<center style="color:var(--fontColor)"><big><big>${content}</big></big></center>`);
-  },
+  }).bind(globalThis),
 };
