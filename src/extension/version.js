287e0317fa914732bb6d05617eb103944f809959
Nyaser Project <admin@nyaser.tk>
2024-06-12 17:56:16 +0000

    ver2024-06-13T00:00

--- src/extension/version.js
+++ src/extension/version.js
@@ -11,6 +11,10 @@ var o = [];
     {
         d: '2024-06-10T18:00',
         m: '修复版本号错误的问题'
+    },
+    {
+        d: '2024-06-13T00:00',
+        m: '新增“每日壁纸”选项（默认开启，可打开 Wallpaper Engine 进行设置）'
     }
 ].forEach(v => o = o.concat({
     date: new Date(v.d + 'Z' ?? 0),
