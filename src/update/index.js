6dffea0529bc24032c37a48182c6c287806b9118
Nyaser Project <admin@nyaser.tk>
2024-06-14 09:03:47 +0000

    debug

--- src/update/index.js
+++ src/update/index.js
@@ -1,6 +1,7 @@
 import update from './update.js'
 
 // Avoid duplicate runs
+if ('undefined' != typeof ec.extension) ec.updater = ec.extension;
 if ('undefined' != typeof ec.updater) delete ec.updater;
 else throw new Error('Duplicate Runs Not Allowed.');
 
