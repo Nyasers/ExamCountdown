1b986fdfec8c2dc7dd07175a23303858a92df89f
Nyaser <Nyasers@outlook.com>
2025-02-04 21:34:13 +0800

    feat: 将设置页面的取消按钮改为关闭

--- src-editor/src/App.vue
+++ src-editor/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <pre hidden>{{ formData }}</pre>
-  <vue-form v-model="formData" :schema="schema"></vue-form>
+  <vue-form v-model="formData" :schema="schema" :formFooter="formFooter"></vue-form>
 </template>
 
 <script>
@@ -15,14 +15,16 @@ const editorWindow = getCurrentWindow();
 export default {
   name: 'Config Editor',
   components: { VueForm },
-  data() { return { formData, schema } },
+  data() { return { formData, schema, formFooter: { cancelBtn: "关闭" } } },
   mounted() {
     this.$nextTick(() => {
       const cancel_button = document.querySelector("#app > form > div.formFooter_item > div > div > div > div > div > button.ant-btn.ant-btn-default");
       const submit_button = document.querySelector("#app > form > div.formFooter_item > div > div > div > div > div > button.ant-btn.ant-btn-primary");
 
       cancel_button.onclick = () => editorWindow.close();
-      submit_button.onclick = () => set_config(this.formData).then(() => alert("已保存"));
+      submit_button.onclick = () => set_config(this.formData)
+        .then(() => alert("保存成功"))
+        .catch((e) => alert("保存失败\n\n" + e));
     });
   },
 };
