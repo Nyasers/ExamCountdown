a19c23016d0f37796d86b09dc413d4352c99760f
Nyaser <Nyasers@outlook.com>
2025-02-04 14:55:59 +0800

    feat: 添加更新插件和相关配置，优化发布工作流

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -48,6 +48,7 @@ fn get_wallpaper_data() -> Result<Vec<u8>, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             None,
