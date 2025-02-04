b2ca01313d7f5f94fcb2d4333eb8c50c57fa4c60
Nyaser <Nyasers@outlook.com>
2025-02-04 19:30:17 +0800

    refactor: 删除不再使用的更新脚本和配置文件

--- src-main/src/cmd/update.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference='Stop';$TempFile=(New-Guid).toString()+'.zip';
-Invoke-WebRequest -Uri 'https://ec.nyase.ru/ExamCountdown.zip' -OutFile $TempFile;
-Expand-Archive -Path $TempFile -DestinationPath . -Force;
-Remove-Item $TempFile;
