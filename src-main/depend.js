82ab612d6a24403b7125bc1b538a90710d130cb8
Nyaser <Nyasers@outlook.com>
2025-02-17 14:45:24 +0800

    feat: 优化依赖处理逻辑，支持空依赖对象，更新重定向链接

--- src-main/depend.js
+++ src-main/depend.js
@@ -1,28 +1,14 @@
 import fs from 'fs';
-// import { spawn } from 'child_process';
 
 fs.readFile('package.json', 'utf-8', (err, data) => {
     if (err) throw err;
     const pkg = JSON.parse(data);
     var args = ['install'];
-    Object.keys(pkg.dependencies).forEach(e => {
+    Object.keys(pkg.dependencies ?? []).forEach(e => {
         args.push(e)
     });
-    Object.keys(pkg.devDependencies).forEach(e => {
+    Object.keys(pkg.devDependencies ?? []).forEach(e => {
         args.push(e)
     });
     console.info(`npm ${args.join(' ')}`);
-    // shell("npm", args);
 });
-
-// function shell(cmd, args) {
-//     const ls = spawn(cmd, args);
-
-//     ls.stdout.on('data', (data) => {
-//         console.log(`${data}`);
-//     });
-
-//     ls.stderr.on('data', (data) => {
-//         console.error(`${data}`);
-//     });
-// }
