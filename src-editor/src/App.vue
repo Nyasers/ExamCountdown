8f663cb80b4a90b254a02a38651da79764be1d3a
Nyaser <Nyasers@outlook.com>
2025-02-17 20:46:12 +0800

    feat: 优化设置界面保存提示

--- src-editor/src/App.vue
+++ src-editor/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <vue-form v-model="formData" :schema="schema" :formFooter="formFooter" :formProps="{
-    layoutColumn: 3
-  }"></vue-form>
+  <vue-form v-model="formData" :schema="schema" :formProps="formProps" :formFooter="formFooter"
+    @validation-failed="handlerValidationFailed" @submit="handlerSubmit" @cancel="handlerCancel"></vue-form>
 </template>
 
 <script>
+import { JsonSchemaFormAntdV4 as VueForm } from "@lljj/vue3-form-ant";
+import { getCurrentWindow } from '@tauri-apps/api/window';
 import schema from "./schema.json";
 import { get_config, set_config } from './store.js';
-import { getCurrentWindow } from '@tauri-apps/api/window';
-import { JsonSchemaFormAntdV4 as VueForm } from "@lljj/vue3-form-ant";
 
 const formData = await get_config();
 const editorWindow = getCurrentWindow();
@@ -16,17 +15,19 @@ const editorWindow = getCurrentWindow();
 export default {
   name: 'Config Editor',
   components: { VueForm },
-  data() { return { formData, schema, formFooter: { cancelBtn: "关闭" } } },
-  mounted() {
-    this.$nextTick(() => {
-      const cancel_button = document.querySelector("#app > form > div.formFooter_item > div > div > div > div > div > button.ant-btn.ant-btn-default");
-      const submit_button = document.querySelector("#app > form > div.formFooter_item > div > div > div > div > div > button.ant-btn.ant-btn-primary");
-
-      cancel_button.onclick = () => editorWindow.close();
-      submit_button.onclick = () => set_config(this.formData)
-        .then(() => alert("保存成功"))
-        .catch((e) => alert("保存失败\n\n" + e));
-    });
+  methods: {
+    handlerValidationFailed(e) {
+      e.forEach(e => this.$message.warning(`${schema.properties[e.name[0]].title} ${e.errors[0]}`))
+    },
+    handlerSubmit() {
+      set_config(this.formData).then(this.$message.success.bind(this, "保存成功"))
+    },
+    handlerCancel() {
+      editorWindow.close()
+    },
+  },
+  data() {
+    return { formData, schema, formProps: { layoutColumn: 3 }, formFooter: { cancelBtn: "关闭" } }
   },
 };
 </script>
