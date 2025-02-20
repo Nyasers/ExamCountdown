5df5eb64dcb83a91d65922b6da2da661d65def20
Nyaser <Nyasers@outlook.com>
2025-02-20 22:43:41 +0800

    chore: 优化 minify.js 执行效率

--- src-editor/minify.js
+++ src-editor/minify.js
@@ -2,9 +2,12 @@ import fs from "fs";
 import path from "path";
 
 const file = path.resolve("dist", "index.min.html");
-var code = fs.readFileSync(file).toString();
-while (code.includes("\\n ")) code = code.replace("\\n ", "\\n");
-while (code.includes(",\\n")) code = code.replace(",\\n", ",");
-while (code.includes("`\\n")) code = code.replace("`\\n", "`");
-while (code.includes("\\n`")) code = code.replace("\\n`", "`");
-fs.writeFileSync(file, code);
+fs.readFile(file, "utf-8", (err, data) => {
+    if (err) throw err;
+    const code = data
+        .replace(/\\n(\s+)/g, '\\n')
+        .replace(/,\\n/g, ',')
+        .replace(/`\\n/g, '`')
+        .replace(/\\n`/g, '`');
+    fs.writeFile(file, code, "utf-8", process.exit);
+});
