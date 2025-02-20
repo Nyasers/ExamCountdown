6255853f4947d2ccb8323d1d41526475e32ad2ab
Nyaser <Nyasers@outlook.com>
2025-02-20 23:28:03 +0800

    Update version.cmd

--- version.cmd
+++ version.cmd
@@ -5,8 +5,5 @@ npm version --no-commit-hooks --no-git-tag-version %version%
 npm run postbuild
 git add .
 git commit -m %version%
-git pull
-git push
 git tag -d v%version%
 git tag v%version%
-git push origin --tags
