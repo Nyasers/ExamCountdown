8ebe7a80c7bfd862c7079b4c387e51d2248d966f
Nyaser <Nyasers@outlook.com>
2025-02-21 14:06:56 +0800

    25.2.22

--- version.cmd
+++ version.cmd
@@ -1,4 +1,4 @@
 @echo off
 set /p version=Input version:
 git add .
-npm version --allow-same-version --no-commit-hooks --no-git-tag-version %version% && npm run postbuild && git add . && git commit -m %version% && git tag -d v%version% && git tag v%version%
+npm version --allow-same-version --no-commit-hooks --no-git-tag-version %version% && npm run postbuild && git add . && git commit -m %version% && git tag v%version%
