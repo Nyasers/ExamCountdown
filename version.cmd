ff51880eb8d9ceec4e01fc6e5cf702baa3413fe6
Nyaser <Nyasers@outlook.com>
2025-05-19 04:53:43 +0800

    update version.cmd

--- version.cmd
+++ version.cmd
@@ -1,4 +1,5 @@
 @echo off
 set /p version=Input version:
 git add .
-npm version --allow-same-version --no-commit-hooks --no-git-tag-version %version% && npm run postbuild && git add . && git commit -m %version% && git tag v%version%
+powershell -c npm version --allow-same-version --no-commit-hooks --no-git-tag-version %version%
+npm run postbuild && git add . && git commit -m %version% && git tag v%version% && git push origin --tags
