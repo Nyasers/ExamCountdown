408cc04d9bc02e9b47e0c53d402496cc76ad5a52
Nyaser <Nyasers@outlook.com>
2025-05-19 03:12:40 +0800

    chore: update dep

--- depend.mjs
+++ depend.mjs
@@ -1,13 +1,11 @@
 import fs from 'fs'
 import child_process from 'child_process';
-import { buffer } from 'stream/consumers';
 
 const args = process.argv.slice(2)
 
 fs.readFile('package.json', 'utf-8', (err, data) => {
     if (err) throw err
-    const pkg = JSON.parse(data), spawn_args = []
-    spawn_args.push('install')
+    const pkg = JSON.parse(data), spawn_args = ['-c', 'npm', 'install']
     args.forEach(e => spawn_args.push(e))
     Object.keys(pkg.dependencies ?? []).forEach(e => {
         spawn_args.push(e)
@@ -15,7 +13,7 @@ fs.readFile('package.json', 'utf-8', (err, data) => {
     Object.keys(pkg.devDependencies ?? []).forEach(e => {
         spawn_args.push(e)
     })
-    const cp = child_process.spawn('npm', spawn_args)
+    const cp = child_process.spawn('powershell', spawn_args)
     cp.stdout.on('data', buffer => console.log(buffer.toString()))
     cp.stderr.on('data', buffer => console.error(buffer.toString()))
 });
